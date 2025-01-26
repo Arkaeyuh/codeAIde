f34140808eaffec7933458028caa51d00e71e6d2
Alex Greene <archaea@ark-2.local>
2025-01-25 18:04:48 -0600

    idek what i changed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function LoginPage() {
+export default function LoginPage() {                                  
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
